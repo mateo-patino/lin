3b8ab0748a8d0dacf6d151bd9c4e106878ee7eae
Mateo Patino <mateopatinodev@gmail.com>
2026-08-06 11:18:16 -0500

    Add ASSERT macro for matrix data

--- tests/test_assert.c
+++ tests/test_assert.c
@@ -21,15 +21,14 @@ void assert_eq_scalar_failed(scalar_t actual, scalar_t expected,
 }
 
 
-/* Helper to the ASSER_EQ_MATDATA macro */
-static bool equal_matrix_data(const scalar_t *actual, const scalar_t *expected, size_t sz) {
-    for (size_t i = 0; i < sz; i++) {
-        if (!is_close(*actual, *expected, 1e-12, 1e-7)) {
-            return false;
-        }
-    }
-    return true;
-} 
-
-
+void assert_eq_matdata_failed(const scalar_t *actual, const scalar_t *expected, size_t sz, const char *file, 
+                          int line, const char *func) {
+    perr(BOLD "%s " ANSI_RED "FAILED" ANSI_RESET "\n", func);
+    perr("  Expected entries: ");
+    print_matrix_data(stderr, actual, sz, true);
+    perr("  Actual entries: ");
+    print_matrix_data(stderr, expected, sz, true);
+    perr("   at %s:%i\n", file, line);
+    perr("   in %s\n", func);
+}
 
--- tests/test_assert.h
+++ tests/test_assert.h
@@ -5,6 +5,7 @@
 #include <stdlib.h>
 
 #include "types/matrix.h"
+#include "test_helpers.h"
 
 /* Assert `expr` is true */
 void assert_true_failed(const char *expr, const char *file, int line, const char *func);
@@ -33,15 +34,25 @@ void assert_eq_scalar_failed(scalar_t actual, scalar_t expected,
 
 
 /* Assert the arrays `actual` and `expected` are identical. */
-static bool equal_matrix_data(const scalar_t *actual, const scalar_t *expected, size_t sz);
-void assert_eq_matdata_failed(scalar_t *actual, scalar_t *expected, size_t sz, const char *file, 
+
+/* Helper to the ASSER_EQ_MATDATA macro */
+static bool equal_matrix_data(const scalar_t *actual, const scalar_t *expected, size_t sz) {
+    for (size_t i = 0; i < sz; i++) {
+        if (!is_close(*actual, *expected, 1e-12, 1e-7)) {
+            return false;
+        }
+    }
+    return true;
+} 
+
+void assert_eq_matdata_failed(const scalar_t *actual, const scalar_t *expected, size_t sz, const char *file, 
                             int line, const char *func);
 #define ASSERT_EQ_MATDATA(actual, expected, sz) \
     do { \
         scalar_t *_actual = actual; \
         scalar_t *_expected = expected; \
         size_t _sz = sz; \
-        if (!equal_matrix_data(_actual, _expected, _sz)) { \ 
+        if (!equal_matrix_data(_actual, _expected, _sz)) { \
             assert_eq_matdata_failed(_actual, _expected, _sz, __FILE__, __LINE__, __func__); \
             return false; \
         } \
