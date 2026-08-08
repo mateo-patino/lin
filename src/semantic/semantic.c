3ae06516c99bb63460b581416ed3dfd4a1684995
Mateo Patino <mateopatinodev@gmail.com>
2026-08-08 01:09:15 -0500

    Add sub, div, and mul checkers

--- src/semantic/semantic.c
+++ src/semantic/semantic.c
@@ -1,6 +1,7 @@
 #include "semantic.h"
 #include "types/matrix.h"
-#include "errorprinter.h"
+
+#include <math.h>
 
 
 semantic_status is_semantically_valid_ast(const ast_t *ast) {
@@ -9,6 +10,10 @@ semantic_status is_semantically_valid_ast(const ast_t *ast) {
 }
 
 
+bool add_overflow(scalar_t a, scalar_t b) {
+    return isinf(a + b);
+}
+
 
 semantic_status valid_add_operands(const token_t *a, const token_t *b) {
     if (!a || !b) {
@@ -16,7 +21,9 @@ semantic_status valid_add_operands(const token_t *a, const token_t *b) {
     }
 
     if (a->type == SCALAR && b->type == SCALAR) {
-        return SEMANTIC_OK; 
+        const scalar_t *sca = (const scalar_t *)a->obj;
+        const scalar_t *lar = (const scalar_t *)b->obj;
+        return add_overflow(*sca, *lar) ? SEMANTIC_FP_OVERFLOW : SEMANTIC_OK; 
     }
     else if (a->type == MATRIX && b->type == MATRIX) {
         const matrix_t *mat = (const matrix_t *)a->obj;
@@ -26,4 +33,24 @@ semantic_status valid_add_operands(const token_t *a, const token_t *b) {
     }
     
     return SEMANTIC_INCOMPATIBLE_OPERANDS;    
+}
+
+
+semantic_status valid_sub_operands(const token_t *a, const token_t *b) {
+    /* Addition and subtraction have the same conditions to be valid operations */
+    return valid_add_operands(a, b) ;
+}
+
+
+semantic_status valid_mul_operands(const token_t *a, const token_t *b) {
+    /* TODO */
+}
+
+
+semantic_status valid_div_operands(const token_t *a, const token_t *b) {
+    if (!a || !b) {
+        return SEMANTIC_NULL_ARGS;
+    }
+    /* TODO: check for NaN */
+    return a->type == SCALAR && b->type == SCALAR ? SEMANTIC_OK : SEMANTIC_INCOMPATIBLE_OPERANDS;
 }
--- src/semantic/semantic.h
+++ src/semantic/semantic.h
@@ -13,6 +13,10 @@
 *
 * A set of enum codes is provided to describe in detail what kind of semantic
 * error was found.
+*
+* In general, functions here do not use the global errorprinter module. Setting
+* error messages is left to the caller who should have access to inputs and the 
+* semantic status returned by the functions here.
 */
 
 #include "parser/ast.h"
@@ -21,15 +25,14 @@
 #include <stdbool.h>
 
 
-
 /* Error codes */
 typedef enum {
     SEMANTIC_OK,
     SEMANTIC_INCOMPATIBLE_OPERANDS,
+    SEMANTIC_FP_OVERFLOW,
     SEMANTIC_UNEQUAL_DIMENSIONS,
     SEMANTIC_NULL_ARGS
 } semantic_status;
-
 
 
 /*
@@ -39,6 +42,16 @@ typedef enum {
 */
 semantic_status is_semantically_valid_ast(const ast_t *ast);
 
+
+/*
+* Returns true if the floating-point addition `a` + `b` gives either 
+* +infinity or -infinity (i.e. floating-point overflow).
+*
+* Note that floating-point overflow is also defined to account for 
+* a large negative value.
+*/
+bool add_overflow(scalar_t a, scalar_t b);
+
 /*
 * Returns SEMANTIC_OK if `a` and `b` are valid operands for addition.
 *
@@ -46,6 +59,26 @@ semantic_status is_semantically_valid_ast(const ast_t *ast);
 * so only scalar-with-scalar and matrix-with-matrix addition is allowed.
 */
 semantic_status valid_add_operands(const token_t *a, const token_t *b);
+
+/*
+* Returns SEMANTIC_OK if `a` and `b` are valid operands for substraction
+*
+* Like with addition, only scalar-with-scalar and matrix-with-matrix subtraction
+* is allowed.
+*/
+semantic_status valid_sub_operands(const token_t *a, const token_t *b);
+
+
+/*
+* Returns SEMANTIC_OK if `a` and `b` are valid multiplication operands.
+*/
+semantic_status valid_mul_operands(const token_t *a, const token_t *b);
+
+/*
+* Returns SEMANTIC_OK if `a` and `b` are valid operands for division.
+* Matrix divison is not a thing, so `a` and `b` must be scalars.
+*/
+semantic_status valid_div_operands(const token_t *a, const token_t *b);
 
 
 #endif
