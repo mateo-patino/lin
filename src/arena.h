61297ef3dfc21a7c122bdc4e376e7c777f9e8d64
Mateo Patino <mateopatinodev@gmail.com>
2026-08-15 21:03:41 -0500

    arena: add alignment macros

--- src/arena.h
+++ src/arena.h
@@ -14,9 +14,30 @@
 * meant to be used exclusively by the functions in this module. Instead,
 * callers must interact with this library via the public functions 
 * provided.
+*
+* The arena is aligned using max_align_t, an alignment value defined in
+* stddef.h which equals the largest alignment required to align every 
+* object type in a system. Note that an alignment of, say, 16 correctly
+* aligns types with alignments of 1, 2, 4, 8 because these are multiples 
+* 16. max_align_t is sort of like 16 in this example. It is a value that
+* large enough to align every other possible type.
+*
+* NEEDSWORK: using max_align_t adds a lot of padding bytes to the 
+* arena's memory block. We could reduce memory usage by using alignof()
+* and having callers pass an alignment value that is specific to the type
+* they want to allocate.
 */
 
 #include <stdlib.h>
+#include <stddef.h>
+#include <stdalign.h>
+
+
+/* Maximum aligned required to allocate any type */
+#define ALIGNMENT alignof(max_align_t)
+
+/* Align `val` up to ALIGNMENT */
+#define ALIGN_UP(val) (((val) + ALIGNMENT - 1) & ~(ALIGNMENT - 1))
 
 typedef struct { 
     char *start;
